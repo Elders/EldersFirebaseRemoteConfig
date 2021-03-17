5540d8099f63feac9a19baa7d47ba0005b4bc004
Milen Halachev <milenski88@gmail.com>
2021-03-17 17:17:34 +0200

    Initial Commit

---
+++ EldersFirebaseRemoteConfig/EldersFirebaseRemoteConfig.h
@@ -0,0 +1,18 @@
+//
+//  EldersFirebaseRemoteConfig.h
+//  EldersFirebaseRemoteConfig
+//
+//  Created by Milen Halachev on 17.03.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EldersFirebaseRemoteConfig.
+FOUNDATION_EXPORT double EldersFirebaseRemoteConfigVersionNumber;
+
+//! Project version string for EldersFirebaseRemoteConfig.
+FOUNDATION_EXPORT const unsigned char EldersFirebaseRemoteConfigVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EldersFirebaseRemoteConfig/PublicHeader.h>
+
+
